5cb4fd8d5a4f59d67b5458203d270735823dd063
Daniel Azil <danielazil.fse.dl@gmail.com>
2024-08-22 12:00:00 +0000

    Implemented changes for 0-calcul.test.js

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,23 @@
+const assert = require("assert");
+const { it, describe } = require("mocha");
+const calculateNumber = require("./0-calcul");
+
+describe("calculateNumber()", function() {
+
+    it("should return 4 when adding 1 and 3", function() {
+      const res = calculateNumber(1, 3);
+      assert.strictEqual(res, 4);
+    });
+    it("should return 5 when adding 1 and 3.7", function() {
+      const res = calculateNumber(1, 3.7);
+      assert.strictEqual(res, 5);
+    });
+    it("should return 5 when adding 1.2 and 3.7", function() {
+      const res = calculateNumber(1.2, 3.7);
+      assert.strictEqual(res, 5);
+    });
+    it("should return -6 when adding 1.5 and 3.7", function() {
+      const res = calculateNumber(1.5, 3.7);
+      assert.strictEqual(res, -6);
+    });
+});
